6c1ee59c9da46f7b1aa36e72252d23a758ee7ada
Doug Herman <douglash9882@gmail.com>
2021-08-23 13:07:10 -0500

    forgot parenthesis on Math.random

--- JavaScript/pizzaOven.js
+++ JavaScript/pizzaOven.js
@@ -13,16 +13,17 @@ console.log(handTossed)
 var kMcAllister = pizzaOven('thin', 'traditional', 'mozzarella', 'none');
 var jimmyPesto = pizzaOven('neopolitan','pesto', ['mozzarella','asiago'], ['arugala,', 'basil', 'cherry tomatoes']);
 
-var crusts = ['thin',"deep dish", "neopolitan"];
-var sauces = ['traditional','pesto','olive oil'];
+var crustType = ['thin',"deep dish", "neopolitan"];
+var sauceType = ['traditional','pesto','olive oil'];
 var cheeses = ['mozzarella', 'gorgonzola','asiago', 'cheddar'];
 var toppings = ['sausage','pepperoni','mushrooms','olives'];
 
 function randomPizza(){
     return pizzaOven (
-    crusts[Math.floor(Math.random * crusts.length)],
-    sauces[Math.floor(Math.random * sauces.length)],
-    cheeses[Math.floor(Math.random * cheeses.length)],
-    toppings[Math.floor(Math.random * toppings.length)]);
+    crustType[Math.floor(Math.random() * crustType.length)],
+    sauceType[Math.floor(Math.random() * sauceType.length)],
+    cheeses[Math.floor(Math.random() * cheeses.length)],
+    toppings[Math.floor(Math.random() * toppings.length)]
+    );
 }
 console.log(randomPizza());
