2176f6bad7c5b4eed0da6afb30ee8d3942b9bd39
Doug Herman <douglash9882@gmail.com>
2021-08-23 21:46:46 -0500

    Likes assignment

---
+++ JavaScript/Likes/script.js
@@ -0,0 +1,20 @@
+var neilLikes = 9
+
+function neilLikeAdd(){
+    neilLikeCounter.innerText = neilLikes + 1 + " like(s)";
+    neilLikes++;
+}
+
+var nicholeLikes = 12;
+
+function nicholeLikeAdd(){
+    nicholeLikeCounter.innerText = nicholeLikes + 1 + " like(s)";
+    nicholeLikes++;
+}
+
+var jimLikes = 9;
+
+function jimLikeAdd(){
+    jimLikeCounter.innerText = jimLikes + 1 + " like(s)";
+    jimLikes++;
+}
