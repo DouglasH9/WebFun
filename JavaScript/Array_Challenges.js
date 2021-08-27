6d95fdf0bda3cb3ca4f1aac3328299e8a4105027
Doug Herman <douglash9882@gmail.com>
2021-08-27 17:09:06 -0500

    fixed fib case of 1 and 0, uncommented previous code

--- JavaScript/Array_Challenges.js
+++ JavaScript/Array_Challenges.js
@@ -1,62 +1,63 @@
 // Array Challenges
 
 // Challenge 1. Always Hungry
-// var arr1 = [3.14, 'food', 'pie', true, 'food'];
-// var arr2 = [1,2,3,4];
-// function alwaysHungry (arr){
-//     var num = 0;
-//     for (var i = 0; i < arr.length; i++){
-//         if (arr[i] == 'food'){
-//             console.log('yummy');
-//         }
-//         else if (arr[i] !== 'food'){
-//             num++;
-//         }
-//     }
-//     if (num == arr.length){
-//         console.log ("I'm hungry");
-//     }
-// }
 
-// alwaysHungry(arr1);
-// alwaysHungry(arr2);
+var arr1 = [3.14, 'food', 'pie', true, 'food'];
+var arr2 = [1,2,3,4];
+function alwaysHungry (arr){
+    var num = 0;
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] == 'food'){
+            console.log('yummy');
+        }
+        else if (arr[i] !== 'food'){
+            num++;
+        }
+    }
+    if (num == arr.length){
+        console.log ("I'm hungry");
+    }
+}
+
+alwaysHungry(arr1);
+alwaysHungry(arr2);
 
 // Challenge 2. High Pass Filter
 
-// function highPass(arr, cutoff) {
-//     var filteredArr = [];
-//     // your code here
-//     for (var i = 0; i<arr.length; i++){
-//         if (arr[i] > cutoff){
-//             filteredArr.push(arr[i]);
-//         }
-//     }
-//     return filteredArr;
-// }
-// var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
-// console.log(result); // we expect back [6, 8, 10, 9]
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    // your code here
+    for (var i = 0; i<arr.length; i++){
+        if (arr[i] > cutoff){
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
 
 // Challenge 3. Better than average
 
-// var sum = 0;
-// var avg = 0;
-// var count = 0;
-// function betterThanAverage(arr) {
-//     // calculate the average
-//     for (var i = 0; i< arr.length; i++){
-//         sum += arr[i];
-//     }
-//     avg = sum/arr.length;
-//     for (var i = 0; i < arr.length; i++){
-//         if (arr[i] > avg){
-//             count++;
-//         }
-//     }
-//     return count;
-// }
+var sum = 0;
+var avg = 0;
+var count = 0;
+function betterThanAverage(arr) {
+    // calculate the average
+    for (var i = 0; i< arr.length; i++){
+        sum += arr[i];
+    }
+    avg = sum/arr.length;
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] > avg){
+            count++;
+        }
+    }
+    return count;
+}
 
-// var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
-// console.log(result); // we expect back 4
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
 
 
 // Challenge 4. Array reverse
@@ -64,20 +65,20 @@
 // var temp = []; universal variable not needed;
 
 
-// function reverse(arr) {
-//     for (var left = 0; Math.floor(left < arr.length/2); left++){
-//         var right = arr.length - 1 - left;
-//         var temp = arr[left];
-//         arr[left] = arr[right];
-//         arr[right] = temp;
-//         console.log('loopnumber' + left + ' ' + arr);
-//     }
-//     // your code here
-//     return arr;
-// }
+function reverse(arr) {
+    for (var left = 0; Math.floor(left < arr.length/2); left++){
+        var right = arr.length - 1 - left;
+        var temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        console.log('loopnumber' + left + ' ' + arr);
+    }
+    // your code here
+    return arr;
+}
 
-// var result = reverse(["a", "b", "dangus", "d", "e"]);
-// console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+var result = reverse(["a", "b", "dangus", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
 
 
 // Challenge 5. Fibonacci Array
@@ -86,10 +87,18 @@
 function fibonacciArr(n){
     var arr = [0,1];
     // var num = 0;
+    if (n==1){
+        arr = [0,1];
+    }
+    else if (n==0){
+        arr = [0];
+    }
+    else {
         for (var i = 0; i <= n; i++){
             arr.push(arr[arr.length - 1]+arr[arr.length - 2]);
         }
+    }
     return arr;
 }
-var result =  fibonacciArr(5);
+var result =  fibonacciArr(0);
 console.log(result);
